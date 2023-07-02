0b9d6a0007eb7b8dcfa9218821ae04f1f5a4a994
biancapbaptista <31194209+biancapbaptista@users.noreply.github.com>
2023-07-02 17:03:13 -0300

    first commit

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/bianca_ios/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bianca_ios/Documents/Flutter/todo_list/consultor_tasks_pk"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
