5ccf131f150212a2bdd2f488eefc5673eecb7c7d
biancapbaptista <31194209+biancapbaptista@users.noreply.github.com>
2023-07-03 15:59:19 -0300

    altercao para formatacao

--- lib/consultor_tasks_pk.dart
+++ lib/consultor_tasks_pk.dart
@@ -1,32 +1,8 @@
 library consultor_tasks_pk;
 
-import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class CustomButton extends StatelessWidget {
-  var onPressed;
-  final Widget child;
-  var style;
-  CustomButton(
-      {required Key key,
-      @required this.onPressed,
-      required this.child,
-      this.style})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.task_alt),
-      onPressed: onPressed,
-      style: TextButton.styleFrom(
-        padding: const EdgeInsets.all(16.0),
-        primary: Colors.white,
-        backgroundColor: Colors.blue,
-        elevation: 9.0,
-        textStyle: const TextStyle(
-          fontSize: 20,
-        ),
-      ),
-    );
-  }
+String formattedPhone(double bmi) {
+  final formatter = NumberFormat('(##)####-####');
+  return formatter.format(bmi);
 }
