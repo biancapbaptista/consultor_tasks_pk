5bbeac5b25168fa80e1e5fd716e7dd9c2cdf7d41
biancapbaptista <31194209+biancapbaptista@users.noreply.github.com>
2023-07-03 16:12:55 -0300

    altercao para formatacao

--- lib/consultor_tasks_pk.dart
+++ lib/consultor_tasks_pk.dart
@@ -1,8 +1,39 @@
 library consultor_tasks_pk;
 
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 
-String formattedPhone(double bmi) {
-  final formatter = NumberFormat('(##)####-####');
-  return formatter.format(bmi);
+class CustomButton extends StatefulWidget {
+  var onPressed;
+  final Widget child;
+  var style;
+  CustomButton(
+      {required Key key,
+      @required this.onPressed,
+      required this.child,
+      this.style})
+      : super(key: key);
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  Color _iconColor = Colors.green;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: IconButton(
+      icon: Icon(Icons.star, color: _iconColor),
+      onPressed: () {
+        setState(() {
+          if (_iconColor == Colors.grey) {
+            _iconColor = Colors.green;
+          } else {
+            _iconColor = Colors.grey;
+          }
+        });
+      },
+    ));
+  }
 }
